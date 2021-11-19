99ef5476833de994dc4fe39eedf2b5cc2156b0a8
seki <seki.ryohei0721@gmail.com>
2021-11-19 17:59:54 +0900

    親から子コンポーネントへデータを送る

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div id="app">
+    <hello-world :title="message" />
+    <hr>
+    <button class="btn btn-primary" v-on:click="doAction">change title</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: 'HELLO'
+    }
+  },
+  methods: {
+    doAction() {
+      var input = prompt("new title");
+      this.message = input;
+    }
+  }
+}
+</script>
