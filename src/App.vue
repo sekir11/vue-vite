cb335ea8fe2f365cd151b8bc828d3ec2a06f249c
seki <seki.ryohei0721@gmail.com>
2021-11-19 19:11:13 +0900

    簡単な計算アプリ作成

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,52 @@
 
   <img alt="Vue logo" src="./assets/logo.png" />
   <div id="app">
-    <hello-world :title="message" v-on:result-event="appAction" />
-    <hr>
-    <p>{{result}}</p>
+    <Calc :title="message" v-on:result-event="appAction" />
+    <div class="mt-3 text-left">
+      <table class="table" v-html="log"></table>
+    </div>
+    <div>
+      <button class="btn btn-danger" v-on:click="doClear">Clear Log</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calc
   },
   data() {
     return {
-      message: 'HELLO',
-      result: 'no event'
+      message: 'CALC',
+      result: []
+    }
+  },
+  computed: {
+    log() {
+      let table = '<tr><th>Expression</th><th>Value</th></tr>';
+      if (this.result.length > 0) {
+        for(var i in this.result) {
+          table += '<tr><td>' + this.result[i][0] + '</td><th>' + this.result[i][1] + '</th></tr>'        
+          }
+      }
+      return table;
     }
   },
   methods: {
-    appAction(message) {
-      this.result = '(*** you send:"' + message + '". ***)';
+    appAction(exp, res) {
+      this.result.unshift([exp, res]);
+      var log = JSON.stringify(this.result);
+      localStorage.setItem('log', log);
+    },
+    doClear() {
+      if (confirm('ログを全て削除します。')) {
+        localStorage.removeItem('log');
+        this.result = [];
+      }
     }
   }
 }
